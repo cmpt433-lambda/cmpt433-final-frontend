2fb49d71a9b7791d5f7583e4230658e1f491214e
Alexander Li (cla383) <cla383@sfu.ca>
2024-03-08 21:04:44 -0800

    build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // <=== enables static exports
+  basePath: "/nextjs-github-pages",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
