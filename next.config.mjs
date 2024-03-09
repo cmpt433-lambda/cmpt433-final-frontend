5ce2358a2fb5ab0858096d151be95ec72030e467
Alexander Li (cla383) <cla383@sfu.ca>
2024-03-08 21:07:48 -0800

    updated base url

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // <=== enables static exports
+  basePath: "/cmpt433-final-frontend",
   reactStrictMode: true,
   images: {
     unoptimized: true,
